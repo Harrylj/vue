8e413b76ab64fd7bac5f9ce68eb894a8a2ec6d6a
Harrylj <18200166593@163.com>
2018-04-28 13:54:13 +0800

    2018-04-28-13:54

--- sell/src/components/header/header.vue
+++ sell/src/components/header/header.vue
@@ -31,7 +31,16 @@
       <div class="background">
         <img :src="seller.avatar" width="100%" height="100%">
       </div>
-      <div v-show="detailShow" class="detail"></div>
+      <div v-show="detailShow" class="detail">
+        <div class="detail-wrapper clearfix">
+          <div class="detail-main">
+            <p>{{seller.bulletin}}{{seller.bulletin}}{{seller.bulletin}}</p>
+          </div>
+        </div>
+        <div class="detail-close">
+          <div class="icon-close"></div>
+        </div>
+      </div>
     </div>
 </template>
 
@@ -181,4 +190,17 @@ export default {
       height 100%
       overflow auto
       background rgba(7, 17, 27, 0.8)
+      .detail-wrapper
+        min-height 100%
+        .detail-main
+          maigin-top 64px
+          padding-bottom 64px
+      .detail-close
+        position relative
+        width 32px
+        height 32px
+        marigin -64px auto 0 auto
+        clear both
+        font-size 32px
+
 </style>
