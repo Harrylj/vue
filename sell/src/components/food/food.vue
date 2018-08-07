916491193b68873d2f013221c6ab9f4da549c188
Harrylj <18200166593@163.com>
2018-08-07 18:41:46 +0800

    2018-08-07-18:41

--- sell/src/components/food/food.vue
+++ sell/src/components/food/food.vue
@@ -1,6 +1,6 @@
 <template>
   <transition name="move">
-    <div class="food" v-show="showFlag">
+    <div class="food" v-show="showFlag" ref="food">
       <div class="food-content">
         <div class="image-header">
           <img :src="food.image">
@@ -16,8 +16,24 @@
           </div>
           <div class="price">
             <span class="now">&yen;{{food.price}}</span>
-            <span class="old" v-show="food.oldPrice">&yen;{{food.oldPrice}}</span>        
+            <span class="old" v-show="food.oldPrice">&yen;{{food.oldPrice}}</span>
           </div>
+          <div class="cartcontrol-wrapper">
+            <cartcontrol :food="food"></cartcontrol>
+          </div>
+          <transition name="fade">
+          <div @click.stop.prevent="addFirst" class="buy" v-show="!food.count || food.count === 0">加入购物车</div>
+          </transition>
+        </div>
+        <split v-show="food.info"></split>
+        <div class="info" v-show="food.info">
+          <h1 class="title">商品信息</h1>
+          <p class="text">{{food.info}}</p>
+        </div>
+        <split></split>
+        <div class="rating">
+          <h1 class="title">商品评价</h1>
+          <ratingselect :select-type="selectType" :only-content="onlyContent" :desc="desc" :ratings="food.ratings"></ratingselect>
         </div>
       </div>
     </div>
@@ -25,6 +41,14 @@
 </template>
 
 <script type="text/ecmascript-6">
+import BScroll from 'better-scroll'
+import Vue from 'vue'
+import cartcontrol from '../../components/cartcontrol/cartcontrol'
+import split from '../../components/split/split'
+import ratingselect from '../../components/ratingselect/ratingselect'
+// const POSITIVE = 0
+// const NEGATIVE = 1
+const ALL = 2
 export default {
   props: {
     food: {
@@ -33,18 +57,54 @@ export default {
   },
   data () {
     return {
-      showFlag: false
+      showFlag: false,
+      selectType: ALL,
+      onlyContent: true,
+      desc: {
+        ALL: '全部',
+        POSITIVE: '推荐',
+        NEGATIVE: '吐槽'
+      }
     }
   },
   methods: {
     show () {
       this.showFlag = true
+      this.selectType = ALL
+      this.onlyContent = true
+      // 绑定滚动
+      this.$nextTick(() => {
+        if (!this.scroll) {
+          this.scroll = new BScroll(this.$refs.food, {
+            click: true
+          })
+        } else {
+          this.scroll.refresh()
+        }
+      })
     },
     hide () {
       this.showFlag = false
+    },
+    //
+    addFirst (event) {
+      // 防止多次点击
+      if (!event._constructed) {
+        return
+      }
+      if (!this.food.count) {
+        Vue.set(this.food, 'count', 1)
+      } else {
+        this.food.count++
+      }
+      console.log(event.target)
+      // this.$dispatch('cart.add',event.target)
     }
   },
   components: {
+    cartcontrol,
+    split,
+    ratingselect
   }
 }
 </script>
@@ -83,4 +143,74 @@ export default {
           font-size 20px
           color #fff
           transform rotate(180deg)
+    .content
+      position relative
+      padding 18px
+      .title
+        line-height 14px
+        margin-bottom 8px
+        font-size 14px
+        font-weight 700
+        color rgb(7,17,27)
+      .detail
+        margin-bottom 18px
+        line-height 10px
+        font-size 0
+        .sell-count,.rating
+          font-size 10px
+          color rgb(147,153,159)
+        .sell-count
+          margin-right 12px
+      .price
+        font-weight 700
+        line-height 24px
+        .now
+          margin-right 8px
+          font-size 14px
+          color rgb(240,20,20)
+        .old
+          text-decoration line-through
+          font-size 14px
+          color rgb(147,153,159)
+    .cartcontrol-wrapper
+      position absolute
+      right 12px
+      bottom 12px
+    .buy
+      position absolute
+      right 18px
+      bottom 18px
+      z-index 10
+      height 24px
+      line-height 24px
+      padding 0 12px
+      box-sizing border-box
+      border-radius 12px
+      font-size 10px
+      color #fff
+      background #00a0dc
+      &.fade-transition
+        transition all 0.2s
+        opacity 1
+      &.fade-enter, &.fade-leave
+        opacity 0
+    .info
+      padding 18px
+      .title
+        line-height 14px
+        margin-bottom 6px
+        font-size 14px
+        color rgb(7,17,27)
+      .text
+        line-height 24px
+        padding 0 8px
+        font-size 12px
+        color rgb(77,85,93)
+    .rating
+      padding-top 18px
+      .title
+        line-height 14px
+        margin-left 18px
+        font-size 14px
+        color rgb(7,17,27)
 </style>
---
+++ sell/src/components/ratingselect/ratingselect.vue
@@ -0,0 +1,84 @@
+<template>
+ <div class="ratingselect">
+   <div class="rating-type border-1px">
+     <span class="block positive">{{desc.ALL}}<span class="count">47</span></span>
+     <span class="block positive">{{desc.POSITIVE}}<span class="count">40</span></span>
+     <span class="block negative">{{desc.NEGATIVE}}<span class="count">7</span></span>
+   </div>
+   <div class="switch">
+     <span class="iconfont"></span>
+     <span class="text">查看评论内容</span>
+   </div>
+ </div>
+</template>
+
+<script type="text/ecmascript-6">
+// const POSITIVE = 0
+// const NEGATIVE = 1
+const ALL = 2
+export default {
+  props: {
+    ratings: {
+      type: Array,
+      default () {
+        return []
+      }
+    },
+    selectType: {
+      type: Number,
+      default: ALL
+    },
+    onlyContent: {
+      type: Boolean,
+      default: false
+    },
+    desc: {
+      type: Object,
+      default () {
+        return {
+          POSITIVE: '满意',
+          NEGATIVE: '不满意',
+          ALL: '全部'
+        }
+      }
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  components: {
+  }
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin"
+  .ratingselect
+    .rating-type
+      padding 18px 0
+      margin 0 18px
+      border-1px(rgba(7,17,27,0.1))
+      font-size 0
+      .block
+        display inline-block
+        padding 8px 12px
+        margin-right 8px
+        border-radius 1px
+        font-size 12px
+        color rgb(77,85,93)
+        line-height 16px
+        &.active
+          color #fff
+        .count
+          margin-left 2px
+          font-size 10px
+        &.positive
+          background rgba(0,160,220,0.2)
+          &.active
+            background rgb(0,160,220)
+        &.negative
+          background rgba(77,85,93,0.2)
+          &.active
+            background rgb(77,85,93)
+</style>
