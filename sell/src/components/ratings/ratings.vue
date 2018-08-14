c8ddf24fdd429d98ed6abe73de16b7408402d60b
Harrylj <18200166593@163.com>
2018-08-14 19:00:31 +0800

    2018-08-14-19:00

--- sell/src/components/ratings/ratings.vue
+++ sell/src/components/ratings/ratings.vue
@@ -1,19 +1,180 @@
 <template>
-    <div class="header">
-        我是ratings{{msg}}
+    <div class="ratings">
+      <div class="ratings-content">
+        <div class="overview">
+          <div class="overview-left">
+            <h1 class="score">{{seller.score}}</h1>
+            <div class="title">综合评分</div>
+            <div class="rank">高于周边商家{{seller.rankRate}}%</div>
+          </div>
+          <div class="overview-right">
+            <div class="score-wrapper">
+              <span class="title">服务态度</span>
+              <star :size="36" :score="seller.serviceScore"></star>
+              <span class="score">{{seller.serviceScore}}</span>
+            </div>
+            <div class="score-wrapper">
+              <span class="title">商品评分</span>
+              <star :size="36" :score="seller.foodScore"></star>
+              <span class="score">{{seller.foodScore}}</span>
+            </div>
+            <div class="delivery-wrapper">
+              <span class="title">送达时间</span>
+              <span class="delivery">{{seller.deliveryTime}}分钟</span>
+            </div>
+          </div>
+        </div>
+        <split></split>
+        <ratingselect @select-type="selectTypeVal" @only-content="onlyContentVal" :ratings="ratings" :onlyContent="onlyContent"></ratingselect>
+        <div class="rating-wrapper">
+          <ul>
+            <li v-for="(rating,index) in ratings" v-bind:key="index"  class="rating-item">
+              <div class="avatar">
+                <img width="28" height="28" :src="rating.avatar">
+              </div>
+              <div class="content">
+                <h1 class="name">{{rating.username}}</h1>
+                <div class="star-wrapper">
+                  <star :size="24" :score="rating.score"></star>
+                  <span class="delivery" v-show="rating.deliveryTime">{{rating.deliveryTime}}</span>
+                </div>
+                <p class="text">{{rating.text}}</p>
+                <div class="recommend" v-show="rating.recommend && rating.recommend.length">
+                  <span class="iconfont icon-damuzhi"></span>
+                  <span v-for="(item,index) in rating.recommend" v-bind:key="index">{{item}}</span>
+                </div>
+                <div class="time">
+                  {{rating.rateTime | formatDate}}
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
 </template>
 
 <script type="text/ecmascript-6">
+import star from '../../components/star/star'
+import split from '../../components/split/split'
+import ratingselect from '../../components/ratingselect/ratingselect'
+import {formatDate} from '../../common/js/date'
+const ALL = 2
+const ERR_OK = 0
 export default {
-  name: 'ratings',
+  props: {
+    seller: {
+      type: Object
+    }
+  },
   data () {
     return {
-      msg: 'ratings!!!Welcome to Your Vue.js App 2018-04-10'
+      ratings: [],
+      selectType: ALL,
+      onlyContent: true
     }
+  },
+  created () {
+    this.$http.get('/api/ratings').then((response) => {
+      response = response.body
+      if (response.errno === ERR_OK) {
+        this.ratings = response.data
+      }
+    })
+  },
+  methods: {
+    // 接收子组件的传值
+    selectTypeVal: function (type) {
+      this.selectType = type
+    },
+    // 接收子组件的传值
+    onlyContentVal (bb) {
+      this.onlyContent = bb
+    }
+  },
+  filters: {
+    // 日期格式化 ---需要引入date.js文件
+    formatDate (time) {
+      let date = new Date(time)
+      return formatDate(date, 'yyyy-MM-dd hh:mm')
+    }
+  },
+  components: {
+    star,
+    split,
+    ratingselect
   }
 }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin"
+  .ratings
+    position absolute
+    top 174px
+    bottom 0
+    left 0
+    width 100%
+    overflow hidden
+    .overview
+      display flex
+      padding 18px 0
+      .overview-left
+        flex 0 0 137px
+        padding 6px 0
+        width 137px
+        border-right 1px solid rgba(7,17,27,0.1)
+        text-align center
+        @media only screen and (max-width: 320px)
+          flex 0 0 120px
+          width 120px
+        .score
+          margin-bottom 6px
+          line-height 28px
+          font-size 24px
+          color rgb(255,153,0)
+        .title
+          margin-bottom 8px
+          line-height 12px
+          font-size 12px
+          color rgb(7,17,27)
+        .rank
+          line-height 10px
+          font-size 10px
+          color rgb(147,153,159)
+      .overview-right
+        flex 1
+        padding 6px 0 6px 24px
+        @media only screen and (max-width: 320px)
+          padding-left 6px
+        .score-wrapper
+          margin-bottom 8px
+          font-size 0
+          .title
+            display inline-block
+            vertical-align top
+            font-size 12px
+            line-height 18px
+            color rgb(7,17,27)
+          .star
+            display inline-block
+            margin 0 12px
+            line-height 18px
+            vertical-align top
+          .score
+            display inline-block
+            line-height 18px
+            vertical-align top
+            font-size 12px
+            color rgb(255,153,0)
+        .delivery-wrapper
+          font-size 0
+          .title
+            font-size 12px
+            line-height 18px
+            color rgb(7,17,27)
+          .delivery
+            margin-left 10px
+            font-size 12px
+            color rgb(147,153,159)
 </style>
